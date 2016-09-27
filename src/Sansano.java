0f7eafe73071ae7b39ac127ce7aaf78d58da6598
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-09-27 15:01:31 -0300

    hecho jugador sansano

--- src/Sansano.java
+++ src/Sansano.java
@@ -1,2 +1,28 @@
 public class Sansano {
+
+    protected final String name;
+    protected int prioridad;
+
+    private Sansano(String nombre) {
+        name = nombre;
+        prioridad = 3000;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void actualizar(int valorCambio) {
+        prioridad += valorCambio;
+        if (prioridad > 3000) {
+            prioridad = 3000;
+        }
+        else if (prioridad < 0){
+            prioridad = 0;
+        }
+    }
 }
