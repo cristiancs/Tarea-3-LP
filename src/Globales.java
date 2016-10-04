177d33914987dbdefc746b658d47015ee9c09e0a
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-10-04 17:14:22 -0300

    eliminadas cartas a mano

--- src/Globales.java
+++ src/Globales.java
@@ -3,29 +3,7 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Globales {
-    /*
-    // Crear cartas tipo Curso
-    static final Curso mate = new Curso("Matemáticas", "Ataca 550/Cura 200 puntos de prioridad.", 550, 200);
-    static final Curso fis = new Curso ("Física", "Ataca 450/Cura 150 puntos de prioridad.", 450, 150);
-    static final Curso lp = new Curso ("LP", "Ataca 510/Cura 180 puntos de prioridad.", 510, 180);
-    static final Curso progra = new Curso("Programación", "Ataca 110/Cura 300 puntos de prioridad.", 110, 300);
-    static final Curso ed = new Curso ("ED", "Ataca 470/Cura 160 puntos de prioridad.", 470, 160);
-    static final Curso edd = new Curso("EDD", "Ataca 430/Cura 120 puntos de prioridad.", 430, 120);
-
-    // Crear cartas tipo Profesor
-    static final Profesor bah = new Profesor("Bahamondes", "Reduce 420 puntos de prioridad.", 420);
-    static final Profesor ma = new Profesor("MaxAraya", "Reduce 350 puntos de prioridad.", 350);
-    static final Profesor cif = new Profesor("Cifuentes", "Reduce 390 puntos de prioridad.", 390);
-    static final Profesor mr = new Profesor("MaxRivera", "Reduce 280 puntos de prioridad.", 280);
-
-    // Crear cartas tipo Carrete
-    static final Carrete cerr = new Carrete("Cerrito", "Recupera 55 puntos de prioridad.", 55);
-    static final Carrete inter = new Carrete("InterMechón", "Recupera 80 puntos de prioridad.", 80);
-    static final Carrete fonda = new Carrete("Sansafonda", "Recupera 100 puntos de prioridad.", 100);
-    static final Carrete semana = new Carrete("SemanaSansana", "Recupera 150 puntos de prioridad.", 150);
-    static final Carrete libre = new Carrete("BloqueLibre", "Recupera 30 puntos de prioridad.", 30);
-    static final Carrete ombligo = new Carrete("FiestaOmbligo", "Recupera 125 puntos de prioridad.", 125);*/
-
+    
     public enum Cards {
         // Crear cartas de Curso
         MATE("Matemáticas", "Ataca 550/Cura 200 puntos de prioridad.", 550, 200),
