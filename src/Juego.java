303ea65fd54a9d70308926d30ce6c197b683f9a8
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-10-04 19:08:28 -0300

    eliminado testeo

--- src/Juego.java
+++ src/Juego.java
@@ -1,10 +1,6 @@
 public class Juego {
 
     public static void main(String[] args) {
-        Sansano jugador = new Sansano("asd");
-        Sansano pc = new Sansano("Oponente");
-        UnJugador prueba = new UnJugador();
-        prueba.asignarCartas(jugador, pc, 1);
         ModoJuegoView portada =  new ModoJuegoView();
 
     }
