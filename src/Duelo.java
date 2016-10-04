4ffbf6e77a31e6c6f0a5721f52c92718635c90a7
CristiaN <cristiannavarrete@outlook.com>
2016-10-04 14:43:25 -0300

    clase duelo

--- src/Duelo.java
+++ src/Duelo.java
@@ -1,2 +1,21 @@
 public class Duelo {
+    private int turno;
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public String getGanador() {
+        return ganador;
+    }
+
+    public void setGanador(String ganador) {
+        this.ganador = ganador;
+    }
+
+    private String ganador;
 }
