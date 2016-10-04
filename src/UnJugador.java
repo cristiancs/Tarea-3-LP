54daeb263004690a21bb68b3e52a35545a60e56d
CristiaN <cristiannavarrete@outlook.com>
2016-10-04 16:53:09 -0300

    crear maso agresivo / defensivo

--- src/UnJugador.java
+++ src/UnJugador.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
+import java.lang.reflect.Array;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Comparator;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Cristian Navarrete on 27-09-16.
  */
 public class UnJugador implements ModoJuego{
+    private Sansano pc = new Sansano();
     public void init (ModoJuegoView oldScreen) {
         oldScreen.setVisible(false);
         PlayView view = new PlayView();
@@ -31,7 +35,7 @@ public class UnJugador implements ModoJuego{
         }
         view.setNombreJugador(nombre);
         if(opcion == 0){
-            System.out.println("Agresivo");
+            Agresivo();
         }
         else if(opcion == 1){
             Defensivo();
@@ -107,21 +111,100 @@ public class UnJugador implements ModoJuego{
             }
         }
     }
-    private void Defensivo(){
-        // Insertar cartas jugador
-        List listaCartas = new ArrayList();
-        for (Globales.Cards cartas : Globales.Cards.values()) {
-            if (cartas != Globales.Cards.BAH && cartas != Globales.Cards.MA && cartas != Globales.Cards.CIF && cartas != Globales.Cards.MR && cartas != Globales.Cards.CERR && cartas != Globales.Cards.INTER && cartas != Globales.Cards.FONDA && cartas != Globales.Cards.WEEK && cartas != Globales.Cards.FREE && cartas != Globales.Cards.OMBLIGO) {
-                listaCartas.add(0, new Curso(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos(), cartas.getDef()));
-                Collections.sort(listaCartas, new Comparator<Curso>() {
-                    @Override
-                    public int compare(Curso a, Curso b) {
-                        // -1 - less than, 1 - greater than, 0 - equal, all inversed for descending
-                        return a.getDefensa() > b.getDefensa() ? -1 : (a.getDefensa() < a.getDefensa()) ? 1 : 0;
-                    }
-                });
 
+    private void giveCartasNormales(List listaCartas){
+        int i;
+        for (Globales.Cards cartas : Globales.Cards.values()) {
+            switch (cartas) {
+                case MATE:
+                    listaCartas.add(0, new Curso(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos(), cartas.getDef()));
+                    break;
+                case FIS:
+                    for (i = 0; i < 4; i++) {
+                        listaCartas.add(0, new Curso(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos(), cartas.getDef()));
+                    }
+                    break;
+                case LP:
+                    for (i = 0; i < 2; i++) {
+                        listaCartas.add(0, new Curso(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos(), cartas.getDef()));
+                    }
+                    break;
+                case PROGRA:
+                    for (i = 0; i < 6; i++) {
+                        listaCartas.add(0, new Curso(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos(), cartas.getDef()));
+                    }
+                    break;
+                case ED:
+                    for (i = 0; i < 3; i++) {
+                        listaCartas.add(0, new Curso(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos(), cartas.getDef()));
+                    }
+                    break;
+                case EDD:
+                    for (i = 0; i < 4; i++) {
+                        listaCartas.add(0, new Curso(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos(), cartas.getDef()));
+                    }
+                    break;
             }
         }
+    }
+    private void giveCartasEspecialesRandom(List listaCartas){
+        int rand, i, i2;
+        i2 = 20;
+        Globales global = new Globales();
+        for (Globales.Cards cartas : Globales.Cards.values()) {
+            switch (cartas) {
+                case BAH:
+                case MA:
+                case CIF:
+                case MR:
+
+                    rand = ThreadLocalRandom.current().nextInt(0, i2);
+                    listaCartas.add(rand, new Profesor(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos()));
+                    i2++;
+                    break;
+                case CERR:
+                case INTER:
+                case FONDA:
+                case WEEK:
+                case FREE:
+                case OMBLIGO:
+
+                    rand = ThreadLocalRandom.current().nextInt(0, i2);
+                    listaCartas.add(rand, new Carrete(cartas.getNombre(), cartas.getDescripcion(), cartas.getPuntos()));
+                    i2++;
+            }
+
+        }
+
+    }
+    private void Defensivo(){
+        // Insertar cartas jugador
+        List listaCartas = new ArrayList(30);
+        giveCartasNormales(listaCartas);
+        Collections.sort(listaCartas, new Comparator<Curso>() {
+            @Override
+            public int compare(Curso a, Curso b) {
+                return a.getDefensa() > b.getDefensa() ? -1 : (a.getDefensa() < b.getDefensa()) ? 1 : 0;
+            }
+        });
+
+        giveCartasEspecialesRandom(listaCartas);
+        for(int x=0;x<listaCartas.size();x++) {
+            pc.addCard((Carta)listaCartas.get(x));
+        }
+    }
+    private void Agresivo(){
+        // Insertar cartas jugador
+        List listaCartas = new ArrayList(30);
+        giveCartasNormales(listaCartas);
+        Collections.sort(listaCartas, new Comparator<Curso>() {
+            @Override
+            public int compare(Curso a, Curso b) {
+                return a.getAtaque() > b.getAtaque() ? -1 : (a.getAtaque() < b.getAtaque()) ? 1 : 0;
+            }
+        });
+
+        giveCartasEspecialesRandom(listaCartas);
+
     }
 }
